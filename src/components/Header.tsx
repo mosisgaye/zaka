3e3ef05e23a962006a7c415b91770934c583ad22
Moussa <mosisg7@gmail.com>
2024-10-28 12:23:57 +0100

    Description des modifications

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client";
 import Image from 'next/image';
 import { useState } from 'react';
 import Link from 'next/link';
@@ -12,19 +11,16 @@ const Header = () => {
   };
 
   return (
-    <header className="w-full bg-white z-50 ">
+    <header className="w-full bg-white z-50">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         {/* Logo */}
         <div className="flex items-center">
           <Link href="/">
             <div>
-              <Image src="/zaka.png" alt="zaka" width={90} height={75}/>
+              <Image src="/ZAKA77.png" alt="zaka" width={90} height={75} />
             </div>
           </Link>
         </div>
-
-        {/* Language Selector */}
-      
 
         {/* Mobile Menu Toggle */}
         <div className="md:hidden">
@@ -43,62 +39,43 @@ const Header = () => {
 
         {/* Desktop Menu */}
         <nav className="hidden md:flex space-x-6">
-          <Link href="/home">
-            <div className="text-gray-700 hover:text-gray-900">Accueil</div>
-          </Link>
-          <Link href="/athletes">
-            <div className="text-gray-700 hover:text-gray-900">Nos athlètes</div>
-          </Link>
-          <Link href="/services">
-            <div className="text-gray-700 hover:text-gray-900">Nos services</div>
-          </Link>
-          <Link href="/about">
-            <div className="text-gray-700 hover:text-gray-900">A propos de nous</div>
-          </Link>
-          <Link href="/blog">
-            <div className="text-gray-700 hover:text-gray-900">Blog</div>
-          </Link>
+          {['/home', '/athletes', '/services', '/about', '/blog'].map((path, index) => (
+            <Link key={index} href={path}>
+              <div className="text-gray-700 hover:text-red-600 transition duration-300 relative group">
+                <span className="group-hover:underline">{path === '/home' ? 'Accueil' : path.split('/')[1]}</span>
+                <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-red-600 transition-all duration-300 group-hover:w-full"></div>
+              </div>
+            </Link>
+          ))}
         </nav>
 
-
-      <div className="hidden md:block">
+        {/* Language Selector and Contact Button */}
+        <div className="hidden md:flex space-x-4 items-center">
           <select className="border border-gray-300 rounded p-2">
             <option value="fr">FR</option>
             <option value="en">EN</option>
           </select>
+          <Link href="/contact">
+            <button className="bg-black text-white px-4 py-2 rounded hover:bg-red-700 transition duration-300">
+              Contact
+            </button>
+          </Link>
         </div>
       </div>
-
 
       {/* Mobile Menu */}
       {isMenuOpen && (
         <nav className="md:hidden bg-white border-t border-gray-200">
           <ul className="flex flex-col space-y-2 p-4">
-            <li>
-              <Link href="/home">
-                <div className="text-gray-700 hover:text-gray-900">Accueil</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/athletes">
-                <div className="text-gray-700 hover:text-gray-900">Nos athlètes</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/services">
-                <div className="text-gray-700 hover:text-gray-900">Nos services</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/about">
-                <div className="text-gray-700 hover:text-gray-900">A propos de nous</div>
-              </Link>
-            </li>
-            <li>
-              <Link href="/blog">
-                <div className="text-gray-700 hover:text-gray-900">Blog</div>
-              </Link>
-            </li>
+            {['/home', '/athletes', '/services', '/about', '/blog'].map((path, index) => (
+              <li key={index}>
+                <Link href={path}>
+                  <div className="text-gray-700 hover:text-red-600 transition duration-300">
+                    {path === '/home' ? 'Accueil' : path.split('/')[1]}
+                  </div>
+                </Link>
+              </li>
+            ))}
             <li>
               <select className="border border-gray-300 rounded p-2 w-full">
                 <option value="fr">FR</option>
